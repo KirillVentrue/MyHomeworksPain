36ee0cbffcb0095f0ca4b775dba8b703fe1f3bdb
KirillVentrue <109892022+KirillVentrue@users.noreply.github.com>
2022-09-20 23:24:27 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿string[] array1 = new string[4] { "hello", "2", "world", ":-)" };
+﻿string[] array1 = new string[] { "hello", "2", "world", ":-)" };
 string[] array2 = new string[array1.Length];
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
